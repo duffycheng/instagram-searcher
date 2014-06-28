89d00138a56f301c66bca4b9666040b2072bfb2a
Pei Cheng <duffy.cheng@gmail.com>
2014-06-28 17:49:01 +0800

    init

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var photoApp = angular.module("photoApp",[]);
+photoApp.controller("formController",function($scope,$rootScope,$http){
+
+})
+.controller("picDisplayController",function($scope,$rootScope){
+	
+});
